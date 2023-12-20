fcef1831600c27579e068bc8b8b4bfdd8a9af7fc
asupstudent <asupstudent@yandex.ru>
2023-12-20 17:04:56 +0300

    Парсер Jsoup заработал

--- src/main/java/com/asupstudent/App.java
+++ src/main/java/com/asupstudent/App.java
@@ -13,7 +13,18 @@ public class App
         try {
             var document = Jsoup.connect("https://logo6.ru/").get();
             var titleElement = document.selectFirst("title");
+            System.out.println("Заголовок страницы");
             System.out.println(titleElement.text());
+            System.out.println("Заголовки h3");
+            var titleElements = document.select("h3");
+            for(var element: titleElements) {
+                System.out.println(element.text());
+            }
+            System.out.println("Адреса ссылок");
+            var linkElements = document.select("a");
+            for(var element: linkElements) {
+                System.out.println(element.attr("href"));
+            }
         } catch(Exception e) {
             e.printStackTrace();
         }
