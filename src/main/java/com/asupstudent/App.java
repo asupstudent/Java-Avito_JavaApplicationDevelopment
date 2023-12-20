b1b0cba895cdb91489a60d2028a71d38d2cd18f0
asupstudent <asupstudent@yandex.ru>
2023-12-20 16:22:05 +0300

    Парсит заголовок сайта

--- src/main/java/com/asupstudent/App.java
+++ src/main/java/com/asupstudent/App.java
@@ -1,5 +1,7 @@
 package com.asupstudent;
 
+import org.jsoup.Jsoup;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            var document = Jsoup.connect("https://logo6.ru/").get();
+            var titleElement = document.selectFirst("title");
+            System.out.println(titleElement.text());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
