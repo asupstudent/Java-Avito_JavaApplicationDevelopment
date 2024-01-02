74a3126aee1fe13c9f6fa5455fd8e99ab150009c
asupstudent <asupstudent@yandex.ru>
2024-01-02 17:52:49 +0300

    Получает данные с авито

--- src/main/java/com/asupstudent/App.java
+++ src/main/java/com/asupstudent/App.java
@@ -1,32 +1,40 @@
 package com.asupstudent;
 
-import org.jsoup.Jsoup;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        try {
-            var document = Jsoup.connect("https://logo6.ru/").get();
-            var titleElement = document.selectFirst("title");
-            System.out.println("Заголовок страницы");
-            System.out.println(titleElement.text());
-            System.out.println("Заголовки h3");
-            var titleElements = document.select("h3");
-            for(var element: titleElements) {
-                System.out.println(element.text());
-            }
-            System.out.println("Адреса ссылок");
-            var linkElements = document.select("a");
-            for(var element: linkElements) {
-                System.out.println(element.attr("href"));
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
+        String baseUrl = "https://www.avito.ru/kulebaki/orgtehnika_i_rashodniki/printery-ASgBAgICAUSoAoQK?s=1";
+        // List<Card> cards = new ArrayList<>();
+        var document = Jsoup.connect(baseUrl)
+                    .timeout(0)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 YaBrowser/23.11.0.0 Safari/537.36")
+                    .referrer("https://ya.ru/")
+                    .get();
+        // Нашли список карточек
+        Element itemsList = document.selectFirst("div[data-marker=catalog-serp]");
+        //Нашли в списке все карточки
+        Elements items = itemsList.select("div[data-marker=item]");
+        for(Element item: items) {
+            /*НАЗВАНИЕ*/
+            Element title = item.selectFirst("h3[itemprop=name]");
+            /*ОПИСАНИЕ*/
+            Element description = item.selectFirst("meta[itemprop=description]");
+            /*ЦЕНА + ДЕНЕЖНАЯ ЕДИНИЦА*/
+            Element priceContainer = item.selectFirst("p[data-marker=item-price]");
+            Element priceCurrency = priceContainer.selectFirst("meta[itemprop=priceCurrency]");
+            Element priceCount = priceContainer.selectFirst("meta[itemprop=price]");
+            System.out.println(title.text());
+            System.out.println(description.attr("content"));
+            System.out.println(priceCount.attr("content") + " " + priceCurrency.attr("content"));
         }
     }
 }
---
+++ src/main/java/com/asupstudent/Card.java
@@ -0,0 +1,40 @@
+package com.asupstudent;
+
+public class Card {
+    private String title;
+    private String description;
+    private String price;
+    private String currency;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
